52be590e9a39b09e60765a0dbf9c25cbd36dc740
Nikita Monokov <nmonokov@gmail.com>
2023-12-20 01:33:02 +0200

    Fixed dark mode card bug. Updated colors. Fixed word validation error.

--- frontend/src/components/EditModal.tsx
+++ frontend/src/components/EditModal.tsx
@@ -48,7 +48,7 @@ const EditModal: React.FC<AddCardSetModalProps> = ({ isOpen, onClose, onUpsert, 
       setError('All fields are required.');
       return false;
     }
-    const formattedWordFound = words.split('\n').find((word: string) => /\b\w+-\w+\b/.test(word));
+    const formattedWordFound = words.split('\n').find((word: string) => /\b\w+\s*-\s*\w+\b/.test(word));
     if (!formattedWordFound) {
       setError('Add at least one word pair in correct format "example-example"');
       return false;
